a734e213efe4aa7a60d17142cd51f7fba2a0b32c
valii2334 <vali_music_addicted@yahoo.com>
2020-04-06 19:25:29 +0300

    CHORE: add boostrap theme

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,16 +3,21 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
+require("@rails/ujs").start();
+require("turbolinks").start();
+require("@rails/activestorage").start();
+require("channels");
 
-import '../stylesheets/application'
-import 'bootstrap/dist/js/bootstrap'
+import "../stylesheets/application";
+import "bootstrap/dist/js/bootstrap";
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+import "assets/javascripts/sb-admin-2.js";
+import "assets/stylesheets/sb-admin-2.css";
+
+import "@fortawesome/fontawesome-free/js/all";
+import "@fortawesome/fontawesome-free/css/all.css";
