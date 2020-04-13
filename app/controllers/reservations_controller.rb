c2cb4c3924e978124e904ae9521a5cce1f574489
valii2334 <vali_music_addicted@yahoo.com>
2020-04-13 20:06:11 +0300

    CHORE: twilio client to send message

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -6,17 +6,17 @@ class ReservationsController < ApplicationController
   load_and_authorize_resource
 
   def destroy
-    company_id = @reservation.company.id
-    reservation_phone_number = @reservation.phone_number
+    duped_reservation = @reservation.dup
+    company = @reservation.company
 
     if @reservation.destroy
-      twiml = Twilio::TwiML::MessagingResponse.new do |r|
-        r.message(
-          from: ENV['TWILIO_NUMBER'],
-          to: reservation_phone_number,
-          body: params[:cancel_reservation_message]
-        )
-      end
+      client.messages.create(
+        from: ENV['TWILIO_NUMBER'],
+        to: duped_reservation.phone_number,
+        body: "#{company.name} reservation from
+               #{duped_reservation.reservation_date.strftime('%d %B, %H:%M %p')}
+               was cancelled with the following message: #{params[:duped_reservation]}"
+      )
     end
 
     redirect_to company_path(id: company_id)
@@ -26,5 +26,9 @@ class ReservationsController < ApplicationController
 
   def set_reservation
     @reservation = Reservation.find(params[:id])
+  end
+
+  def client
+    Twilio::REST::Client.new ENV['ACd2e04b1473ad8ed895b9f98ba38e46dc'], ENV['f9f789def220dc9c0e6d651afe67fed1']
   end
 end
